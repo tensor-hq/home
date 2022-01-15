7022dc99950cfcb5cd4d1793df024dcbe0584c67
ilmoi <iljamoi@protonmail.com>
2022-01-15 10:54:40 +0200

    basic homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="main">
+    <div class="flex justify-center">
+      <AsimovLogo />
+    </div>
+    <h1 class="text-6xl my-5 tracking-widest">asimov</h1>
+    <div class="flex justify-center">
+      <p class="mr-2">NFT appraisal protocol on Solana</p>
+      <SolanaLogo class="mt-1" />
+    </div>
+    <a href="mailto:team@asimov.so" class="text-blue-700">team@asimov.so</a>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import AsimovLogo from "@/components/AsimovLogo.vue";
+import SolanaLogo from "@/components/SolanaLogo.vue";
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
+  components: { SolanaLogo, AsimovLogo },
+  setup() {
+    return {};
   },
 });
 </script>
+
+<style scoped>
+.main {
+  @apply h-screen w-screen mx-auto flex flex-col justify-center align-middle text-center;
+  position: fixed;
+}
+</style>

f7bf3f1ed9db4410e6939d7a6866ca17da9115d5
ilmoi <iljamoi@protonmail.com>
2022-03-21 17:46:23 +0200

    added gtag and twitter meta

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(VueGtag, {
+    config: { id: "G-CQ7WDVCZ2D" },
+  })
+  .mount("#app");
